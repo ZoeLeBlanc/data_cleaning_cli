3585c736f4d4e29f612abd5d70b27be5419f2550
ZoeLeBlanc <zgleblanc@gmail.com>
2023-11-17 15:15:50 -0600

    cleaning up

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# data_cleaning_cli
+# Data Cleaning CLI
+
+## Description
+
+This repository contains some example code for creating data cleaning pipelines using the Rich Library in Python.
+
+Some principles for creating a CLI for data cleaning:
+
+- Try to use the formatting to make the CLI as readable as possible
+- Include a count of how many items you have left to clean
+- Write the output to a file as you are cleaning
--- example_data_cleaning_script.py
+++ example_data_cleaning_script.py
@@ -20,29 +20,7 @@ def display_data(df, console, rows=5):
         table.add_row(*[str(val) for val in row])
     console.print(table)
 
-def remove_missing_values(df):
-    """ Remove rows with missing values """
-    return df.dropna()
-
-def filter_data(df):
-    """ Filter data based on user input """
-    column = Prompt.ask("Enter the column to filter on")
-    if column not in df.columns:
-        console.print(f"[red]Column {column} not found in data[/red]")
-        return df
-
-    value = Prompt.ask(f"Enter the value for filtering {column}")
-    return df[df[column] == value]
-
-def save_data(df, file_path):
-    """ Save the cleaned data to a CSV file """
-    try:
-        df.to_csv(file_path, index=False)
-        console.print(f"[green]Data saved to {file_path}[/green]")
-    except Exception as e:
-        console.print(f"[red]Error saving file: {e}[/red]")
-
-def confirm_name_and_member(df, console):
+def confirm_name_and_member(df, console, output_path):
     """ Confirm if 'name' and 'committee_member' are the same """
     for index, row in df.iterrows():
         if row['name'] != row['committee_member']:
@@ -52,15 +30,17 @@ def confirm_name_and_member(df, console):
                 corrected_name = Prompt.ask("Enter the correct name")
                 df.at[index, 'name'] = corrected_name
                 df.at[index, 'committee_member'] = corrected_name
+                df.to_csv(output_path, index=False)
     return df
 
-def add_research_areas(df, console):
+def add_research_areas(df, console, output_path):
     """ Add additional research areas """
     for index, row in df.iterrows():
         console.print(f"Current Research Area for {row['name']}: {row['research_area']}")
         if Confirm.ask("Do you want to add more research areas?"):
             additional_areas = Prompt.ask("Enter additional research areas, separated by commas")
             df.at[index, 'research_area'] += ", " + additional_areas
+            df.to_csv(output_path, index=False)
     return df
 
 # Main Execution
@@ -77,17 +57,3 @@ if __name__ == '__main__':
 
         df = confirm_name_and_member(df, console)
         df = add_research_areas(df, console)
-
-        if Confirm.ask("Do you want to remove rows with missing values?"):
-            df = remove_missing_values(df)
-            console.print("[green]Missing values removed[/green]")
-            display_data(df, console)
-
-        if Confirm.ask("Do you want to filter the data?"):
-            df = filter_data(df)
-            console.print("[green]Data filtered[/green]")
-            display_data(df, console)
-
-        if Confirm.ask("Do you want to save the cleaned data?"):
-            save_path = Prompt.ask("Enter the path to save the cleaned data")
-            save_data(df, save_path)
